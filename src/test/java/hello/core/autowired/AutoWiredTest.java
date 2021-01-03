8f3d1b7c41a53e4ffc545b1370723fd66af8207a
RATIONAL331 <rational331@gmail.com>
2021-01-03 20:05:13 +0900

    Create AutoWiredTest.java

---
+++ src/test/java/hello/core/autowired/AutoWiredTest.java
@@ -0,0 +1,33 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutoWiredTest {
+    @Test
+    void AutoWiredOption(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(testBean.class);
+        // Member는 Bean으로 등록되지 않은 상태이기 때문에 Autowired의 대상이 되지 못한다.
+    }
+    
+    static class testBean{
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1){
+            System.out.println("noBean1 = " + noBean1);
+        }
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2){
+            System.out.println("noBean2 = " + noBean2);
+        }
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3){
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+}
