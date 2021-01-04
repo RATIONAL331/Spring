58af000324bdb53abb6fd2a6c91bd3ea1e7dabcf
RATIONAL331 <rational331@gmail.com>
2021-01-05 00:20:27 +0900

    scope

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,14 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    //private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> myLoggerProvider; // request는 요청이 들어올 때 생성되므로 생성된 후 가져와야 된다. 그 전에 받아오면(주입) request 가 없으므로 생성 불가능
+    private final MyLogger myLogger; // proxy 사용중
+    //private final ObjectProvider<MyLogger> myLoggerProvider; // request는 요청이 들어올 때 생성되므로 생성된 후 가져와야 된다. 그 전에 받아오면(주입) request 가 없으므로 생성 불가능
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); // 빈의 생성 지연
+        //MyLogger myLogger = myLoggerProvider.getObject(); // 빈의 생성 지연
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("Contoller Test");
